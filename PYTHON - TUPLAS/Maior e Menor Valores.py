092aafc1456c29f7d0c2fbb9f46b9d05c0b3b9f4
Pedro Icaro <josepedroicaro@gmail.com>
2025-12-11 20:34:57 -0300

    Adjust

---
+++ PYTHON - TUPLAS/Maior e Menor Valores.py
@@ -0,0 +1,8 @@
+from random import randint 
+
+n = (randint(1, 10), randint(1,10), randint(1,10),randint(1,10),randint(1,10)) #Randomiza vários valores dentro da tupla
+
+
+print (f"Os números sorteados foram: {n}") 
+print (f"O maior número foi:{max(n)}") #Puxa o maior valor randomizado
+print (f"O menor número foi:{min(n)}") #Puxa o menor valor randomizado
