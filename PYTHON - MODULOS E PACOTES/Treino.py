d2a0771899a61fc9e8ab89b06b924cdca6664fef
Pedro Icaro <josepedroicaro@gmail.com>
2025-12-13 21:48:50 -0300

    Adjust

--- PYTHON - MODULOS E PACOTES/Treino.py
+++ PYTHON - MODULOS E PACOTES/Treino.py
@@ -2,8 +2,8 @@ import moeda
 
 valor = float (input("Digite um preço:"))
 
-print(f"A metade de {valor} é: {moeda.metade(valor)}")
-print(f"O dobro de {valor} é: {moeda.dobro(valor)}")
-print(f"Aumentando o preço em 10% ficará: {moeda.aumentar10(valor, 10)}")
-print(f"Diminuindo o preço em 10% ficará: {moeda.diminuir10(valor, 10)}")
+print(f"A metade de {moeda.moeda(valor)} é: {moeda.moeda(moeda.metade(valor))}")
+print(f"O dobro de {moeda.moeda(valor)} é: {moeda.moeda(moeda.dobro(valor))}")
+print(f"Aumentando o preço em 10% ficará: {moeda.moeda(moeda.aumentar10(valor, 10))}")
+print(f"Diminuindo o preço em 10% ficará: {moeda.moeda(moeda.diminuir10(valor, 10))}")
 
--- PYTHON - MODULOS E PACOTES/moeda.py
+++ PYTHON - MODULOS E PACOTES/moeda.py
@@ -1,16 +1,18 @@
-def metade (n):
+def metade (n = 0):
   res = n / 2
   return res 
 
-def dobro(n):
+def dobro(n = 0):
     res = n * 2
     return res 
 
-def aumentar10(n, f):
+def aumentar10(n = 0 , f = 0):
    res = n + (n * f / 100)
    return res
 
-def diminuir10(n, f):
+def diminuir10(n = 0, f = 0):
    res = n - (n * f / 100)
    return res
 
+def moeda(p=0, m='R$'):
+   return f'{m}{p:.2f}'.replace('.',',') 
